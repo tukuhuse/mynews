98520096fd839a86ae2e4c2676ad7205adfcd4ad
EC2 Default User <ec2-user@ip-172-31-29-213.us-east-2.compute.internal>
2020-02-03 16:10:32 +0000

    課題5終了状態

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -7,5 +7,8 @@ use App\Http\Controllers\Controller;
 
 class NewsController extends Controller
 {
-    //
+    //以下を追記
+    public function add() {
+      return view('admin.news.create');
+    }
 }
