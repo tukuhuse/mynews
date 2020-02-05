88fcea28cc9b800ba16c4a9e6fea4227c23078be
EC2 Default User <ec2-user@ip-172-31-29-213.us-east-2.compute.internal>
2020-02-05 15:36:18 +0000

    課題4

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Profile</title>
+  </head>
+  <body>
+    <h1>Profile作成</h1>
+  </body>
+</html>
---
+++ resources/views/admin/profile/edit.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Profile</title>
+  </head>
+  <body>
+    <h1>Profile編集</h1>
+  </body>
+</html>
