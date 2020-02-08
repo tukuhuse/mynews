1bc5bfdd6882c9ef530bd2e8b7ca9587b3ef95e7
EC2 Default User <ec2-user@ip-172-31-29-213.us-east-2.compute.internal>
2020-02-08 09:40:34 +0000

    課題4終了

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,12 +1,16 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>New Profile</title>
-  </head>
-  <body>
-    <h1>Profile作成</h1>
-  </body>
-</html>
+{{-- layouts/profile/blade.phpを読み込む --}}
+@extends('layouts.profile');
+
+{{-- profile.blade.phpの@yield('title')に'プロフィールの作成'を埋め込む --}}
+@section('title', 'プロフィールの作成')
+
+{{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8 mx-auto">
+        <h2>プロフィールの作成</h2>
+      </div>
+    </div>
+  </div>
+@endsection
