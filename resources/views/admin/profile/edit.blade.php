a09fc828a87ec34b37f449d111d3739c53e62e25
EC2 Default User <ec2-user@ip-172-31-29-213.us-east-2.compute.internal>
2020-02-10 14:59:22 +0000

    lesson21課題1終了

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -1,12 +1,16 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Profile</title>
-  </head>
-  <body>
-    <h1>Profile編集</h1>
-  </body>
-</html>
+{{-- layouts/profile/blade.phpを読み込む --}}
+@extends('layouts.profile');
+
+{{-- profile.blade.phpの@yield('title')に'Profile'を埋め込む --}}
+@section('title', 'Profile')
+
+{{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8 mx-auto">
+        <h2>プロフィールの編集</h2>
+      </div>
+    </div>
+  </div>
+@endsection
