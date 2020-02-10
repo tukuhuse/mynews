7013c85b62241ddfc11a58715d844dd8be4d626a
EC2 Default User <ec2-user@ip-172-31-29-213.us-east-2.compute.internal>
2020-02-10 15:10:29 +0000

    lesson21課題3終了

--- routes/web.php
+++ routes/web.php
@@ -31,4 +31,5 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::group(['prefix' => 'admin'], function() {
   Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
   Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
+  Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
 });
