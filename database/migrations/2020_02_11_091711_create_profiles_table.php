d059d5b956c115a102dc57d401e7740b8590dc82
EC2 Default User <ec2-user@ip-172-31-29-213.us-east-2.compute.internal>
2020-02-11 11:48:22 +0000

    lesson23課題5終了

--- database/migrations/2020_02_11_091711_create_profiles_table.php
+++ database/migrations/2020_02_11_091711_create_profiles_table.php
@@ -6,23 +6,30 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateProfilesTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        //
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('profiles', function (Blueprint $table) {
+      $table->bigIncrements('id');    //オートナンバー
+      $table->string('name');         //名前
+      $table->string('gender');       //性別
+      $table->string('hobby');        //趣味
+      $table->string('introduction'); //自己紹介
+      $table->timestamps();           //保存日時
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        //
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('profiles');
+  }
 }
