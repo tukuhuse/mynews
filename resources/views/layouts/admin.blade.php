66a9cb6055b38c95957df5cf17adde5c40e29351
EC2 Default User <ec2-user@ip-172-31-29-213.us-east-2.compute.internal>
2020-02-17 13:26:02 +0000

    lesson26課題2終了

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -63,7 +63,7 @@
 											{{ __('Logout') }}
 										</a>
 
-										<form id="logout-form" action="{{ route('logout') }}" method="POST" display="none">
+										<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none">
 											@csrf
 										</form>
 									</div>
